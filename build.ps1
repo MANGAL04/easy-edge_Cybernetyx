d1e2fef0c32aaad50d32aa8f11d224ea79b040f1
Raj Hammeer Singh Hada <hammeerraj@gmail.com>
2025-07-16 22:53:19 +0530

    - overhaul

--- build.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Installing dependencies..."
-pip install -r requirements.txt
-pip install pyinstaller
-
-Write-Host "Building easy-edge executable..."
-pyinstaller --onefile --name easy-edge --add-data "models;models" easy_edge.py
-
-Write-Host "Build complete. Executable is in dist\easy-edge.exe" 
